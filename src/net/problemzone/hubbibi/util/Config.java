1611eef0160e9fc9f39e020df1340519f34e21cd
Florian Sprünken <florian.spruenken@gmail.com>
2022-05-05 00:39:10 +0200

    Add Jumpy + Additional Updates

--- src/net/problemzone/hubbibi/util/Config.java
+++ src/net/problemzone/hubbibi/util/Config.java
@@ -1,4 +1,4 @@
-package net.problemzone.hubbibi.utils;
+package net.problemzone.hubbibi.util;
 
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
